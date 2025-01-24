14bbcbbc0b77e2c4f5d9c225054c2a1ddb35e020
Timmy <oyinloyematthew6@gmail.com>
2025-01-23 20:28:44 -1100

    working on the services section

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './Services.css'
+
+const Services = () => {
+
+  return (
+    <div className="services">
+        <div className="studio">
+            <h1>Studio Services</h1>
+        </div>
+
+        <div className="Events">
+            <h1>Events</h1>
+        </div>
+
+        <div className="Artist">
+            <h1>Artist and Collaboration</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Services
