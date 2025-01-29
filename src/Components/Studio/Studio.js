c4a2110aef5a99f9fbaa1207cbc395c5ce0c4280
Timmy <oyinloyematthew6@gmail.com>
2025-01-29 01:24:27 -1100

    studio section background made

--- src/Components/Studio/Studio.js
+++ src/Components/Studio/Studio.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Studio.css';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
+import {faMicrophoneLines} from '@fortawesome/free-solid-svg-icons';
+import {faMusic} from '@fortawesome/free-solid-svg-icons';
+import {faHeadphones} from '@fortawesome/free-solid-svg-icons';
 
 
 const Studio = () =>{
@@ -15,14 +17,16 @@ const Studio = () =>{
             <div className="studio-header">
 
                 <h1>Studio Services</h1>
-                <p> Join Exclusive VVIP of our Studio </p>
+                <p className="paragraph"> Join Exclusive VVIP of our Studio </p>
             </div>
 
             <div className="studio-grid">
 
-                <div className="studio-work">
-                    <FontAwesomeIcon icon="fa-solid fa-microphone-lines" />
-                    <h1>Studio Work</h1>
+                <div className="studio-work box">
+                    <FontAwesomeIcon className='icon' icon={faMicrophoneLines} 
+                    color='rgb(241, 71, 10)'
+                    size="3x"/>
+                    <h3>Studio Work</h3>
                     <p>Our state-of-the-art studio provides a 
                         professional environment with top-notch
                         equipment, ensuring the perfect space for creativity, 
@@ -30,8 +34,12 @@ const Studio = () =>{
                     </p>
                 </div>
 
-                <div className="recording">
-                    <h1>Recording</h1>
+                <div className="recording box">
+                    <FontAwesomeIcon className='icon' icon={faMusic} 
+                                     color='rgb(241, 71, 10)'
+                                     size="3x"
+                    />
+                    <h3>Recording</h3>
                     <p>We offer premium recording services, 
                         capturing crystal-clear vocals and 
                         instruments with advanced technology 
@@ -39,8 +47,12 @@ const Studio = () =>{
                         audio for artists, bands, and creators.</p>
                 </div>
 
-                <div className="production">
-                    <h1>Production Services</h1>
+                <div className="production box">
+                <FontAwesomeIcon className='icon' icon={faHeadphones} 
+                                     color='rgb(241, 71, 10)'
+                                     size="3x"
+                    />
+                    <h3>Production Services</h3>
                     <p>From composing and arranging to mixing 
                        and mastering, our production services 
                        transform ideas into fully produced tracks 
