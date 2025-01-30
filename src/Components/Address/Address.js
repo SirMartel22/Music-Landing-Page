62a647c783f342a0b595c26dc7cd72a29beeda78
Timmy <oyinloyematthew6@gmail.com>
2025-01-30 10:13:33 -1100

    added the address section as the footer

--- src/Components/Address/Address.js
+++ src/Components/Address/Address.js
@@ -12,42 +12,42 @@ const Address = () =>{
 
     return(
         <div className = "address-section">
-            
-            <div className="addresscontainer">
 
-                <div className="address-box">
+                <div className="address-container">
 
-                    <div className="about">
+                    <div className="aboutt box">
                     <FontAwesomeIcon className='icon' icon={faAddressCard} 
                                             color='rgb(241, 71, 10)'
-                                            size="3x"/>
+                                            size="2x"/>
                         <h3>About</h3>
                         <p>Running Guide Workout</p>
                     </div>
 
-                    <div className="Phone">
+                    <div className="Phone box">
 
                     <FontAwesomeIcon className='icon' icon={faPhone} 
                                             color='rgb(241, 71, 10)'
-                                            size="3x"/>
+                                            size="2x"/>
                         <h3>Phone</h3>
                         <p>+222 887 467826</p>
                         <p>+222 887 467826</p>
 
                     </div>
 
-                    <div className="about">
+                    <div className="location box">
                          <FontAwesomeIcon className='icon' icon={faLocationCrosshairs} 
                                             color='rgb(241, 71, 10)'
-                                            size="3x"/>
+                                            size="2x"/>
                         <h3>Our Office Location</h3>
-                        <p>The Interior Design Studio Company,
-                            The Couryard. AI Colorado, USA </p>
+                        <p>The Musical Studio Company,
+                           Ilorin, Nigeria </p>
                     </div>
-
-                </div>
             </div>
             
+            <div className="copyright">
+                <p>© 2025 Musical Website. 
+                    All rights reserved.</p>
+            </div>
         </div>
     )
 }
